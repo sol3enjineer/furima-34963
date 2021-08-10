071ce82d698d6fe840d83f991903eb4f51d517ad
sol3enjineer <f96moribe@gmail.com>
2021-08-10 14:30:27 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,8 @@ class ItemsController < ApplicationController
     before_action :authenticate_user!, except: [:index]
     def index
     end
+
+    def message_params
+        params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    end
 end
