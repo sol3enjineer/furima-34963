fecd230fec1386b534181cac1d11c5a687eed5f4
sol3enjineer <f96moribe@gmail.com>
2021-08-10 13:29:52 +0900

    itemテーブルの作成

---
+++ db/migrate/20210810034708_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name,                  null: false
+      t.references :user,              null: false, foreign_key: true
+      t.text :product_description,     null: false
+      t.integer :category_id,          null: false
+      t.integer :product_condition_id, null: false
+      t.integer :shipping_charge_id,   null: false
+      t.integer :prefecture_id,        null: false
+      t.integer :shipping_date_id,     null: false
+      t.integer :price,                null: false
+      t.timestamps
+    end
+  end
+end
