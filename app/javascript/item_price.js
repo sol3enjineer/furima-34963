d5f2501bff0b3ec7d162d47a6a3a7e34dab44aba
sol3enjineer <f96moribe@gmail.com>
2021-08-12 16:01:55 +0900

    入力するたびにイベントを発火できる

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', () => {
     console.log("OK");
-  });
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      console.log("イベント発火");
+    })
+});
