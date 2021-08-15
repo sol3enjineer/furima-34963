1173ee737c3962604a0423110a71f3a9e9fef0a6
sol3enjineer <f96moribe@gmail.com>
2021-08-15 12:41:46 +0900

    商品情報編集機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-    before_action :authenticate_user!, except: [:index, :show]
-    
+    before_action :authenticate_user!, except: [:index, :show ]
+
     def index
         @items = Item.all
     end
@@ -13,18 +13,34 @@ class ItemsController < ApplicationController
         @item = Item.new(item_params)
         if @item.save
             redirect_to root_path
-          else
+        else
             render :new
         end
     end
+
+    def edit
+        @item = Item.find(params[:id])
+        unless current_user.id == @item.user_id
+            redirect_to action: :index
+        end
+    end
     
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        if @item.save
+            redirect_to root_path
+        else
+            render :edit
+        end
+    end
+
     def show
         @item = Item.find(params[:id])    
     end
 
     private
     def item_params
-        params.require(:item).permit(:image, :name, :product_description, :price, :application_record_id, :category_id, :prefecture_id, :product_condition_id, :shipping_charge_id, :shipping_date_id ).merge(user_id: current_user.id)
+     params.require(:item).permit(:image, :name, :product_description, :price, :application_record_id, :category_id, :prefecture_id, :product_condition_id, :shipping_charge_id, :shipping_date_id ).merge(user_id: current_user.id)
     end
-
 end
