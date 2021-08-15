a9ce14022f90b8255d0365f91ef8cffc7073637c
sol3enjineer <f96moribe@gmail.com>
2021-08-15 20:45:50 +0900

    商品情報編集機能修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show ]
+    before_action :set_item, except: [:index, :new, :create]
+    before_action :update_item, only: [:update, :edit]
 
     def index
         @items = Item.all
@@ -19,14 +21,10 @@ class ItemsController < ApplicationController
     end
 
     def edit
-        unless current_user.id == @item.user_id
-            redirect_to action: :index
-        end
     end
     
     def update
-        @item.update(item_params)
-        if @item
+        if @item.update(item_params)
             redirect_to root_path
         else
             render :edit
@@ -41,11 +39,11 @@ class ItemsController < ApplicationController
      params.require(:item).permit(:image, :name, :product_description, :price, :application_record_id, :category_id, :prefecture_id, :product_condition_id, :shipping_charge_id, :shipping_date_id ).merge(user_id: current_user.id)
     end
 
-    def set_Item
+    def set_item
         @item = Item.find(params[:id])
     end
 
-    def update
+    def update_item
         unless current_user.id == @item.user_id
             redirect_to action: :index
         end
