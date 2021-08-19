9b900cddef76708fd7abd7a524af666d91c38f14
sol3enjineer <f96moribe@gmail.com>
2021-08-19 21:15:02 +0900

    商品購入機能修正6

--- app/models/formobject.rb
+++ app/models/formobject.rb
@@ -17,7 +17,7 @@ class Formobject < ApplicationRecord
 
     def save
         order = Order.create(item_id: item_id, user_id: user_id )
-        Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building_name: building_name, telephone_number: telephone_number, order_id: order_id)
+        Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
     end
 end
 
